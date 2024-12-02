3a1eaf73e07775bf69cba7fa1143e5676e93cfb9
Labyrinth-759 <valenzuelajoshua759@gmail.com>
2024-12-02 19:21:13 +0800

    added new activity

---
+++ app/src/main/java/com/example/tasktidy3D/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.example.tasktidy3D;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
