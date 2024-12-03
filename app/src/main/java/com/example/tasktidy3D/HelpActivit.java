9bcbeca63831f297b53d78f880d8b499e7f1612e
Labyrinth-759 <valenzuelajoshua759@gmail.com>
2024-12-03 12:57:49 +0800

    added help

---
+++ app/src/main/java/com/example/tasktidy3D/HelpActivit.java
@@ -0,0 +1,14 @@
+package com.example.tasktidy3D;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelpActivit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
